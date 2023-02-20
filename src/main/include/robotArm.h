4fcc8c981a313ab374fa896cccb1e061b31cb900
shitpostalotl <terminally.nobody@disroot.org>
2023-02-20 15:43:01 -0800

    robot arm headers

--- src/main/include/robotArm.h
+++ src/main/include/robotArm.h
@@ -1,3 +1,16 @@
+#pragma once
+
+#include <ctre/phoenix/motorcontrol/can/WPI_TalonFX.h>
+#include <ctre/phoenix/sensors/WPI_CANCoder.h>
+#include <frc/kinematics/SwerveModuleState.h>
+#include <units/angular_velocity.h>
+#include <units/time.h>
+#include <units/velocity.h>
+#include <numbers>
+#include <rev/SparkMaxRelativeEncoder.h>
+#include <rev/CANSparkMax.h>
+#include <frc/kinematics/SwerveModulePosition.h>
+
 #include "Constants.h"
 #include "hardwareSettings.h"
 
@@ -6,4 +19,12 @@ class robotArm {
     robotArm(const double arm[]);
 
     private:
+    rev::CANSparkMax m_motorAngleRight;
+    rev::CANSparkMax m_motorAngleLeft;
+    rev::SparkMaxRelativeEncoder m_encoderMotorAngleRight = m_motorAngleRight.GetEncoder(rev::SparkMaxRelativeEncoder::Type::kHallSensor, 42);
+    rev::SparkMaxRelativeEncoder m_encoderMotorAngleLeft = m_motorAngleLeft.GetEncoder(rev::SparkMaxRelativeEncoder::Type::kHallSensor, 42);
+    rev::SparkMaxPIDController m_motorAngleRightController = m_motorAngleRight.GetPIDController();
+    rev::SparkMaxPIDController m_motorAngleLeftController = m_motorAngleLeft.GetPIDController();
+
+    const double m_encoderOffset;
 };

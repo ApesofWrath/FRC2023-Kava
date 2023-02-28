f1896f9f077f60793eac22a23fb1da420946b41a
shitpostalotl <terminally.nobody@disroot.org>
2023-02-27 17:28:37 -0800

    youll never guess yep i missed a spot

--- src/main/cpp/commands/ArmDownLow.cpp
+++ src/main/cpp/commands/ArmDownLow.cpp
@@ -1,14 +1,14 @@
-#include "commands/ArmDownLow.h"
+#include "commands/ArmDownLow.h" // relevent header file
 
-ArmDownLow::ArmDownLow(robotArm* robotArm) : m_robotArm{robotArm} {
-    SetName("ArmDownLow");
-    AddRequirements({m_robotArm});
+ArmDownLow::ArmDownLow(robotArm* robotArm) : m_robotArm{robotArm} { // constructor for command class
+    SetName("ArmDownLow"); // set the ?? name
+    AddRequirements({m_robotArm}); // require the m_robotArm pointer
 }
 
-void ArmDownLow::Initialize() { printf("ArmDownLow Initialized \n"); }
+void ArmDownLow::Initialize() { printf("ArmDownLow Initialized \n"); } // print debug message on initialization
 
-void ArmDownLow::Execute() {
-    m_robotArm->armDownLow();
+void ArmDownLow::Execute() { // ??[when does it do this]
+    m_robotArm->armDownLow(); // make m_robotArm point to the armDown() command
 }
 
-bool ArmDownLow::IsFinished() { return true; }
+bool ArmDownLow::IsFinished() { return true; } // return when ??
